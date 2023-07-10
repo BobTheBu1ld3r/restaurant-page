1c61d91a3e3daa836fb06a8da925547a83e4e557
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-07-10 17:15:59 +0100

    Implement tabbed browsing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./style.css";
+import renderHomePage from "./home.js";
+import renderMenusPage from "./menus.js";
+import renderPrivateDiningPage from "./privateDining.js";
+
+const content = document.querySelector("div.content");
+
+const homeButton = document.querySelector("button.home");
+const menusButton = document.querySelector("button.menus");
+const privateDiningButton = document.querySelector("button.private-dining");
+
+homeButton.addEventListener("click", renderHomePage);
+menusButton.addEventListener("click", renderMenusPage);
+privateDiningButton.addEventListener("click", renderPrivateDiningPage);
+
+renderHomePage();
