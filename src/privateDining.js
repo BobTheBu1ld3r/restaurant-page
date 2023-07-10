f4f789b281514163b24ca1ab6e8f86313c3f5063
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-07-10 17:15:31 +0100

    Add module for private dining page

---
+++ src/privateDining.js
@@ -0,0 +1,7 @@
+export default function renderPrivateDiningPage() {
+  document.querySelector("div.content").innerHTML = `
+    <div>
+      <p>This is the Private Dining page. What a cool page this is!</p>
+    </div>
+    `;
+}
