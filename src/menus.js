05560266c7e1a412348c1e44f7ddac7c55b8e4ce
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-07-10 17:15:15 +0100

    Add content menus page

---
+++ src/menus.js
@@ -0,0 +1,7 @@
+export default function renderMenusPage() {
+  document.querySelector("div.content").innerHTML = `
+  <div>
+    <p>This is the Menus page. What a cool page this is!</p>
+  </div>
+  `;
+}
