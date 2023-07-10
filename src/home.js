5c71d825570f811bcec4766464315eb9ee6ee6f1
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-07-10 17:14:59 +0100

    Add module for home page

---
+++ src/home.js
@@ -0,0 +1,7 @@
+export default function renderHomePage() {
+  document.querySelector("div.content").innerHTML = `
+    <div>
+      <p>This is the Home page. What a cool page this is!</p>
+    </div>
+    `;
+}
