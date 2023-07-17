2160d007064908ac0ec550fbee8ce525a73b7d8b
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-07-17 21:49:47 +0100

    Add renderMenusPage listener to all menu links

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import renderPrivateDiningPage from "./privateDining.js";
 const content = document.querySelector("div.content");
 
 const homeButton = document.querySelector("button.home");
-const menusButton = document.querySelector("button.menus");
+const menusButtons = document.querySelectorAll("button.menus");
 const privateDiningButton = document.querySelector("button.private-dining");
 
 homeButton.addEventListener("click", renderHomePage);
-menusButton.addEventListener("click", renderMenusPage);
+menusButtons.forEach((button) =>
+  button.addEventListener("click", renderMenusPage)
+);
 privateDiningButton.addEventListener("click", renderPrivateDiningPage);
 
 // renderHomePage();
