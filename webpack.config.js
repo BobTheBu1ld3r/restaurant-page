9f3d518c48010df39417cfaa814960bc1b4ab803
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-07-28 18:44:10 +0100

    Configure loader for babel

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,13 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
     ],
   },
   output: {
